a1d12161411d4b24778ff54a03a26b3ba0c25481
karimo123 <kgher047@uottawa.ca>
2022-04-12 19:22:13 -0400

    Fixed movies page wasn't loading

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
  function onSearchFromLanding(event){
     localStorage.setItem("searchedValue", event.target.value)
-    window.location.href = `${window.location.origin}/movies.html`
+    window.location.href = `movies.html`
 }
